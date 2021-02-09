5c437b3f3884f32af1e0c991754f8b979a380a6c
wlaub <goldcray@gmail.com>
2021-02-08 22:16:33 -0800

    additional cleaning

--- main.py
+++ main.py
@@ -11,28 +11,6 @@ Loopmidi needs to be running with an appropriatedly named port
 """
 
 import midi
-import mido
-
-def find_midi(name):
-    """
-    case instensitive contains serach for midi id of a name
-    """
-    names = mido.get_output_names()
-    for match in names:
-        if name.lower() in match.lower(): return match
-    raise KeyError(f'No matching device for \'{name}\' in {names}')
-
-class MidiNote():
-    def __init__(self, note_id, **kwargs):
-        self.note_id = note_id
-        self.start_msg = mido.Message('note_on', **kwargs)
-        self.stop_msg = mido.Message('note_off', **kwargs)
-
-    def start(self, port):
-        port.send(self.start_msg)
-        
-    def stop(self, port):
-        port.send(self.stop_msg)
 
 if __name__ == "__main__":
 
