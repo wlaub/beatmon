7d18e0f64a03a39da0c00b27119241ac7fa3e10f
wlaub <goldcray@gmail.com>
2021-03-04 12:08:40 -0800

    probably changed something

--- sample_render.py
+++ sample_render.py
@@ -14,9 +14,17 @@ for entry in data:
 
     map_name = f"{map_info['songName']} - {map_info['songAuthorName']} - {map_info['levelAuthorName']}"
 
+    cuts = [x for x in events if x['event']=='noteCut']
+    for cut in cuts:
+        info = cut['noteCut']
+#        if not info['speedOK'] or not info['directionOK'] or not info['saberTypeOK'] or info['wasCutTooSoon']:
+        if info['initialScore'] == 0:
+            print(info)
+            print('')
+
     cuts = [x for x in events if x['event']=='noteFullyCut']
 
-    times = [x['time'] for x in cuts]
+    times = [x['noteCut']['noteID'] for x in cuts]
     data_values = {
         'precision': [x['noteCut']['cutDistanceScore'] for x in cuts],
         'score': [x['noteCut']['finalScore'] for x in cuts],
@@ -24,22 +32,24 @@ for entry in data:
         'timing': [x['noteCut']['timeDeviation']*1000 for x in cuts],       
     }
 
-    """
     plt.title('Timing Accuracy')
     plt.scatter(times, data_values['timing'], label = map_info['songName'], s=4)
     plt.xlabel('Cut Time (seconds since epoch)')
     plt.ylabel('Time Deviation (ms)')
-    """
 
-    """
+    
     misses = [x for x in events if x['event']=='noteMissed']
-    times = [x['time'] for x in misses]
+    times = [x['noteCut']['noteID'] for x in misses]
     yvals = [0 for x in misses]
     plt.scatter(times, yvals, c='r', marker='x', s=6)
+    
     """
-
-    plt.hist(data_values['precision'], label=map_info['songName'], density=True, bins=[x-0.5 for x in range(17)])
+#    plt.hist(data_values['precision'], label=map_info['songName'], density=True, bins=[x-0.5 for x in range(17)])
     plt.title('Cut Distance Score Histogram',)
+    key = 'cutDistanceScore'
+    plt.hist([x['noteCut'][key] for x in cuts], label=map_info['songName'], density=True)   
+    plt.title(f'{key} histogram')
+    """
 
 plt.legend()
 
--- session_browser.py
+++ session_browser.py
@@ -38,30 +38,40 @@ class App():
 
         self.buttons.extend([next_song, prev_song])
 
+    def get_plot_set(self):
+        data, map_info = self.get_song_entries(self.selected_map)
+        return data
+
+    def do_plot(self):
+        data = self.get_plot_set()
+        self.axes.clear()
+        for entry in data:
+            self.plot_score(self.axes, entry)
+        plt.draw()
+
     def cycle_song(self, event, inc):
         idx = self.session_hashes.index(self.selected_map)
         idx += inc
         idx %= len(self.session_hashes)
         self.selected_map = self.session_hashes[idx]
 
-        self.axes.clear()
-        data, map_info = self.get_song_entries(self.selected_map)
-        for entry in data:
-            self.draw_timing(self.axes, entry, map_info)
-        plt.draw()
+        self.do_plot()
 
     def get_song_entries(self, selection):
         data = filter(lambda x: x['map_hash'] == selection, self.sessions)
         return data, self.songmap[selection]
 
-    def draw_timing(self, ax, entry, map_info):
+    def plot_timing(self, ax, entry):
+        map_info = self.songmap[entry['map_hash']]
         events = entry['events']
 
-        map_name = f"{map_info['songName']} - {map_info['songAuthorName']} - {map_info['levelAuthorName']}"
+        map_name = f"{map_info['songName']} - {map_info['songAuthorName']}\n{map_info['levelAuthorName']} - {map_info['difficulty']}"
 
         cuts = [x for x in events if x['event']=='noteFullyCut']
 
         times = [x['time'] for x in cuts]
+        mintime = min(times)
+        times = [x - mintime for x in times]
         data_values = {
             'precision': [x['noteCut']['cutDistanceScore'] for x in cuts],
             'score': [x['noteCut']['finalScore'] for x in cuts],
@@ -76,15 +86,37 @@ class App():
         ax.set_ylabel(f'Time Deviation (ms)')
         ax.grid(True)
 
+    def plot_score(self, ax, entry):
+        map_info = self.songmap[entry['map_hash']]
+        events = entry['events']
 
+        map_name = f"{map_info['songName']} - {map_info['songAuthorName']}\n{map_info['levelAuthorName']} - {map_info['difficulty']}"
+
+        cuts = [x for x in events if x['event']=='noteFullyCut']
+
+        times = [x['time'] for x in cuts]
+        mintime = min(times)
+        times = [x - mintime for x in times]
+        data_values = {
+            'precision': [x['noteCut']['cutDistanceScore'] for x in cuts],
+            'score': [x['noteCut']['finalScore'] for x in cuts],
+            'cutscore': [x['noteCut']['finalScore']-x['noteCut']['cutDistanceScore'] for x in cuts],       
+            'timing': [x['noteCut']['timeDeviation']*1000 for x in cuts],       
+        }
+
+        ax.scatter(times, data_values['cutscore'], label = map_info['songName'], s=4)
+
+        ax.set_title(f'Block Score\n{map_name}')
+        ax.set_xlabel(f'Cut Time (seconds since first cut)')
+        ax.set_ylabel(f'Score (0-115)')
+        ax.grid(True)
+        ax.set_ylim(0,115)
+
+   
 
     def run(self):
-        data, map_info = self.get_song_entries(self.selected_map)
-        for entry in data:
-            self.draw_timing(self.axes, entry, map_info)
-
+        self.do_plot()
         plt.show()
-                
 
-app = App('sample_data')
+app = App('sessions')
 app.run()
